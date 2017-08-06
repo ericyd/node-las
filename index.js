3d1f6c1d257b288c57cdb30bab65ff5bf305b81a
Eric <eric@ericyd.com>
2017-08-05 18:28:37 -0700

    refactor variable length records type

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const jBinary = require('jbinary');
 const lasTypeset = require('./lib/binaryTypeset');
 
 jBinary
-  .load('test/data/1.0_0.las', lasTypeset)
+  .load('test/data/malheur-or.las', lasTypeset)
   .then(function(jb /* : jBinary */) {
     // read everything using type aliased in lasTypeset['jBinary.all']
     var data = jb.readAll();
 
     // do something with data in las file
     // data....
-    console.log(data);
+    // console.log(data);
+    console.log(data.variableLengthRecords);
 
     jb.seek(0); // reusing same instance (and memory buffer) by resetting pointer
     // jb.writeAll(data); // writing entire content from data
