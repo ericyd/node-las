11a80da79ca340367f2f4fa9ec2df7b258444d6a
Eric <eric@ericyd.com>
2017-08-07 22:52:43 -0700

    add write methods for VLRs, point data, and EVLRs

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const jBinary = require("jbinary");
-const lasTypeset = require("./lib/binaryTypeset");
-const fs = require("fs");
+const jBinary = require('jbinary');
+const lasTypeset = require('./lib/binaryTypeset');
+const fs = require('fs');
 
 jBinary
-  .load("test/data/malheur-or.las", lasTypeset)
+  .load('test/data/malheur-or.las', lasTypeset)
   .then(function(jb /* : jBinary */) {
     // read everything using type aliased in lasTypeset['jBinary.all']
     var data = jb.readAll();
 
     // do something with data in las file
-    fs.writeFileSync("example-output.js", JSON.stringify(data));
+    fs.writeFileSync('example-output.js', JSON.stringify(data));
 
     jb.seek(0); // reusing same instance (and memory buffer) by resetting pointer
-    // jb.writeAll(data); // writing entire content from data
-    // jb.saveAs('sample.new.las'); // saving file under given name
+    jb.writeAll(data); // writing entire content from data
+    jb.saveAs('sample.new.las'); // saving file under given name
   })
   .catch(console.log.bind(console));
