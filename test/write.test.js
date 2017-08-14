14f8692179f875e299312c78948bd1e6120cfc8c
Eric <eric@ericyd.com>
2017-08-13 17:54:24 -0700

    update write tests to utilize mock-fs to reduce file system io

--- test/write.test.js
+++ test/write.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const R = require('ramda');
+const mockfs = require('mock-fs');
 const las = require('../');
 
 // define helper functions for comparing files
@@ -11,6 +12,10 @@ const getHashByAlgorithm = R.curry((algorithm, hashType, data) =>
 const md5 = getHashByAlgorithm('md5', 'hex');
 const sha256 = getHashByAlgorithm('sha256', 'hex');
 
+afterAll(() => {
+  mockfs.restore();
+});
+
 test('output file should match input file', done => {
   const inputPath = path.join(__dirname, 'data', 'malheur-or.las');
   const outputPath = path.join(__dirname, 'data', 'sample2.las');
@@ -18,12 +23,22 @@ test('output file should match input file', done => {
   const inputMD5 = md5(input);
   const inputSHA256 = sha256(input);
 
-  las.read(inputPath).write(outputPath).then(() => {
-    const output = fs.readFileSync(outputPath);
-    const outputMD5 = md5(output);
-    const outputSHA256 = sha256(output);
-    expect(outputMD5).toEqual(inputMD5);
-    expect(outputSHA256).toEqual(inputSHA256);
-    done();
+  mockfs({
+    'test/data': {
+      'malheur-or.las': input
+    }
   });
+
+  las
+    .read(inputPath)
+    .write(outputPath)
+    .then(() => {
+      const output = fs.readFileSync(outputPath);
+      const outputMD5 = md5(output);
+      const outputSHA256 = sha256(output);
+      expect(outputMD5).toEqual(inputMD5);
+      expect(outputSHA256).toEqual(inputSHA256);
+      done();
+    })
+    .catch(console.log.bind(console));
 });
