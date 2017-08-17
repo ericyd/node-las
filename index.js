f92403c015b8a2b47a140bedc5c6c9b8e51d8ad8
Eric <eric@ericyd.com>
2017-08-17 08:00:58 -0700

    begin adding basic filter function

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const _read = require('./lib/read');
 const _toJSON = require('./lib/toJSON');
 const _write = require('./lib/write');
+const _filter = require('./lib/filter');
+const binaryTypeset = require('./lib/binaryTypeset');
 
 const las = function(x) {
   this.__value = x;
 };
 
-las.of = x => new las(x);
+las.of = function(x) {
+  return new las(x);
+};
 las.read = _read;
+las.prototype.map = function map(f) {
+  return las.of(f(this.__value));
+};
+las.prototype.get = function get() {
+  return this.__value;
+};
 las.prototype.write = _write;
 las.prototype.toJSON = _toJSON;
+// TODO: I think prototyping filter is conflicting with built in filter prototypes;
+// probably need to choose a new name, though I'm not a huge fan of filterData
+las.prototype.filterData = function filterData(options) {
+  return this.map(task => {
+    return task.map(binary => {
+      // versionMajor and minor are 24 and 25 bytes offset from the start of the file, respectively
+      const versionMajor = binary.read('uint8', 24);
+      const versionMinor = binary.read('uint8', 25);
+      // offsetToPointData is 95 bytes offset
+      const offsetToPointData = binary.read('uint32', 96);
+      // number of bytes in the point records
+      const pointDataLength = binary.read('uint16', 105);
+      binary.seek(0);
+      const data = binary.readAll();
+      // TODO: could probably read point data using the point data format declared in the typeset
+      const filteredPoints = _filter(data.pointData);
+      binary.write(
+        // TODO: point data format needs to be dynamic
+        binaryTypeset.pointDataFormat1,
+        filteredPoints,
+        offsetToPointData
+      );
+      return binary;
+    });
+  });
+};
 
 module.exports = las;
